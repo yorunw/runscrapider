1fba341641e8a89b19dee179644c381e310fd1e6
yorun <yorunw@gmail.com>
2018-11-07 18:32:46 +0800

    dev

--- test.py
+++ test.py
@@ -5,28 +5,6 @@ from os import path as opath
 import importlib
 
 
-def _format_nt_path(path):
-    """格式化windows路径为类unix格式"""
-
-    path = re.sub(r'\w?:\\', '/', path)
-    path = re.sub(r'\\', '/', path)
-    return path
-
-
-def _split_base_dir(base_dir, path):
-    """去掉base_dir返回相对路径"""
-
-    if os.name == 'nt':
-        base_dir = _format_nt_path(base_dir)
-        path = _format_nt_path(path)
-    return re.sub(base_dir + '/', '', path)
-
-
-def _path2module(path):
-    """相对路径转换为类模块路径"""
-    return re.sub(r'.py', '', re.sub('/', '.', path))
-
-
 class Spider:
 
     def __init__(self, abs_path):
@@ -36,16 +14,16 @@ class Spider:
         path: c/d
         module_path: c.d
         """
-        self.base_dir = opath.abspath('./work/spiders/')
-        self.abs_path = abs_path
-        self.path = _split_base_dir(self.base_dir, self.abs_path)
-        self.module_path = _path2module(self.path)
+        self._base_dir = opath.abspath('./work/spiders/')
+        self._abs_path = abs_path
+        self._path = self._split_base_dir()
+        self._module_path = self._path2module()
         self.class_name = self.get_spider_class_name()
         self.name = self.get_spider_name()
 
     @property
     def module(self):
-        return importlib.import_module(self.module_path)
+        return importlib.import_module(self._module_path)
 
     def get_spider_class_name(self):
         """返回spider类名
@@ -71,6 +49,26 @@ class Spider:
         cls = getattr(self.module, class_name)
         return cls.name
 
+    @staticmethod
+    def _format_nt_path(path):
+        """格式化windows路径为类unix格式"""
+
+        path = re.sub(r'\w?:\\', '/', path)
+        path = re.sub(r'\\', '/', path)
+        return path
+
+    def _split_base_dir(self):
+        """去掉base_dir返回相对路径"""
+
+        if os.name == 'nt':
+            self._base_dir = self._format_nt_path(self._base_dir)
+            self._abs_path = self._format_nt_path(self._abs_path)
+        return re.sub(self._base_dir + '/', '', self._abs_path)
+
+    def _path2module(self):
+        """相对路径转换为类模块路径"""
+        return re.sub(r'.py', '', re.sub('/', '.', self._path))
+
 
 class Spiders:
     PY_CACHE_DIR = '__pycache__'
@@ -78,8 +76,8 @@ class Spiders:
 
     def __init__(self, spider_dir):
         self.base_dir = opath.abspath(spider_dir)  # spider目录 str
-        self.abs_path_list = []  # spider绝对路径 str
-        self.spiders = []
+        self._abs_path_list = []  # spider绝对路径 str
+        self._spiders = []
 
         sys.path.append(spider_dir)  # 将spider目录加入环境变量
 
@@ -102,19 +100,19 @@ class Spiders:
                         _foo(abs_path)
 
                     if opath.isfile(abs_path):
-                        self.abs_path_list.append(abs_path)
+                        self._abs_path_list.append(abs_path)
 
         _foo(self.base_dir)
-        return self.abs_path_list
+        return self._abs_path_list
 
-    def init_spider(self):
+    def get_spider(self):
         for path in self.get_spider_abs_path():
-            self.spiders.append(Spider(path))
+            self._spiders.append(Spider(path))
 
 
 if __name__ == '__main__':
     s = Spiders('./work/spiders/')
-    s.init_spider()
+    s.get_spider()
 
-    for spider in s.spiders:
+    for spider in s._spiders:
         print(spider.name)
