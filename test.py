4008ba914da3c465fa85ad6dfb3c5d76ca5c9445
yorun <yorunw@gmail.com>
2018-11-07 17:50:18 +0800

    dev

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sys
 import os
 import re
 from os import path as opath
-from importlib import import_module
+import importlib
 
 
 def _format_nt_path(path):
@@ -27,36 +27,73 @@ def _path2module(path):
     return re.sub(r'.py', '', re.sub('/', '.', path))
 
 
-class SpiderInfo:
-    # SPIDER_DIR = './work/spiders/'
+class Spider:
+
+    def __init__(self, abs_path):
+        """
+        base_dir: /a/b
+        abs_path: /a/b/c/d
+        path: c/d
+        module_path: c.d
+        """
+        self.base_dir = opath.abspath('./work/spiders/')
+        self.abs_path = abs_path
+        self.path = _split_base_dir(self.base_dir, self.abs_path)
+        self.module_path = _path2module(self.path)
+        self.class_name = self.get_spider_class_name()
+        self.name = self.get_spider_name()
+
+    @property
+    def module(self):
+        return importlib.import_module(self.module_path)
+
+    def get_spider_class_name(self):
+        """返回spider类名
+        e.g.
+            class DemoSpider(scrapy.Spider):
+                name = 'demo'
+                somecode...
+        :return: DemoSpider -> str
+        """
+        for attr in dir(self.module):
+            if re.search('\w*?Spider', attr):
+                return attr
+
+    def get_spider_name(self):
+        """返回spider启动名
+        e.g.
+            class DemoSpider(scrapy.Spider):
+                name = 'demo'
+                somecode...
+        :return: demo -> str
+        """
+        class_name = self.get_spider_class_name()
+        cls = getattr(self.module, class_name)
+        return cls.name
+
+
+class Spiders:
     PY_CACHE_DIR = '__pycache__'
     PY_INIT_FILE = '__init__.py'
 
     def __init__(self, spider_dir):
-        self.base_dir = opath.abspath(spider_dir)
-        self.path_list = []  # 类模块路径 type:list
+        self.base_dir = opath.abspath(spider_dir)  # spider目录 str
+        self.abs_path_list = []  # spider绝对路径 str
+        self.spiders = []
 
-        self.get_spider_path()
-        sys.path.append(spider_dir)
+        sys.path.append(spider_dir)  # 将spider目录加入环境变量
 
     @classmethod
     def __new__(cls, *args, **kwargs):
-        if not hasattr(SpiderInfo, "_instance"):
-            SpiderInfo._instance = object.__new__(cls)
-        return SpiderInfo._instance
+        if not hasattr(Spider, "_instance"):
+            Spider._instance = object.__new__(cls)
+        return Spider._instance
 
-    def get_spider_path(self):
-        for abs_path in self._get_spider_abs_path():
-            self.path_list.append(_path2module(_split_base_dir(self.base_dir, abs_path)))
-        return self.path_list
-
-    def _get_spider_abs_path(self):
+    def get_spider_abs_path(self):
         """获取spider的绝对路径"""
-        abs_path_list = []
 
         def _foo(parent_dir):
             """迭代文件夹获取路径"""
-
             for path in os.listdir(parent_dir):
                 if path != self.PY_CACHE_DIR and path != self.PY_INIT_FILE:
                     abs_path = opath.join(parent_dir, path)
@@ -65,25 +102,19 @@ class SpiderInfo:
                         _foo(abs_path)
 
                     if opath.isfile(abs_path):
-                        abs_path_list.append(abs_path)
+                        self.abs_path_list.append(abs_path)
 
         _foo(self.base_dir)
-        return abs_path_list
+        return self.abs_path_list
 
-    def import_module(self, path):
-        module = import_module(path)
-        return module
+    def init_spider(self):
+        for path in self.get_spider_abs_path():
+            self.spiders.append(Spider(path))
 
 
 if __name__ == '__main__':
-    s = SpiderInfo('./work/spiders/')
-    # for path in s.path_list:
-    #     print(path)
-    #     m = s.import_module(path)
-    #     print(dir(m))
+    s = Spiders('./work/spiders/')
+    s.init_spider()
 
-    ss = SpiderInfo('./work/spiders')
-    print(id(s))
-    print(id(ss))
-
-    # print(ss.path_list)
+    for spider in s.spiders:
+        print(spider.name)
