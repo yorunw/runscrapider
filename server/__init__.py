5de6784e9d14d9f6a6d90620409b71a150f9b34f
yorun <yorunw@gmail.com>
2018-11-10 17:24:02 +0800

    add move view dist files tool

--- server/__init__.py
+++ server/__init__.py
@@ -1,16 +1,22 @@
 # -*-coding: utf8-*-
 
-from flask import Flask
+import os
+from flask import Flask, render_template, send_from_directory
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/', methods=['GET'])
-    @app.route('/index', methods=['GET'])
+    @app.route('/')
+    @app.route('/index')
     def index():
         """加载react-app"""
-        return "index"
+        return render_template('index.html')
+
+    @app.route('/favicon.ico')
+    def favicon():
+        """将favicon.ico挂到跟路由下"""
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
     from .api import api
     app.register_blueprint(api, url_prefix='/api')
