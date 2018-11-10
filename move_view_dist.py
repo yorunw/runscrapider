8ceb2a99a66f5123add7fbf5af88d5e4d7e46765
yorun <yorunw@gmail.com>
2018-11-10 17:25:02 +0800

    dev

---
+++ move_view_dist.py
@@ -0,0 +1,38 @@
+import os
+from os import path as op
+import shutil
+
+base_dir = op.abspath('..')
+
+dist_dir = op.join(base_dir, 'view/build')
+target_dir = op.join(base_dir, 'server')
+
+
+def copy(dist_name, target_name):
+    dist_path = op.join(dist_dir, dist_name)
+    target_path = op.join(target_dir, target_name)
+
+    if op.isfile(dist_path):
+        if op.exists(target_path):
+            os.remove(target_path)
+
+        shutil.copyfile(dist_path, target_path)
+
+    if op.isdir(dist_path):
+        if op.exists(target_path):
+            shutil.rmtree(target_path)
+
+        shutil.copytree(dist_path, target_path)
+
+
+def copy_static():
+    static_list = os.listdir(op.join(dist_dir, 'static'))
+    for item in static_list:
+        copy('static/' + item, 'static/' + item)
+
+
+copy('index.html', 'templates/index.html')
+copy('favicon.ico', 'static/favicon.ico')
+# copy('static/js', 'static/js')
+# copy('static/css', 'static/css')
+copy_static()
