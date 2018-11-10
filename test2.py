f3df1ced5b08a5bb46cce561b85379ca4b1c89a2
yorun <yorunw@gmail.com>
2018-11-10 18:35:00 +0800

    dev

--- test2.py
+++ test2.py
@@ -37,6 +37,7 @@ def _foo(path, node):
 
 x = []
 
+"""
 for path in path_list:
     f1, o1 = path.split('/', 1)
 
@@ -46,21 +47,53 @@ for path in path_list:
 
         f2, o2 = o1.split('/', 1)
 
-        if not x1:
-            x2 = {f2: []}
-            x1[f1].append(x2)
-
-            f3, o3 = o2.split('/', 1)
-
     else:
-        f2, o2 = o1.split('/', 1)
+        for xx in x:
+            for item in xx.keys():
+                if f1 != item:
+                    x1 = {f1: []}
+                    x.append(x1)
 
-        # f2, o2 = o1.split('/', 1)
-        #
-        if not x1:
+        if not x1[f1]:
             x2 = {f2: []}
             x1[f1].append(x2)
 
             f3, o3 = o2.split('/', 1)
 
+        else:
+            for xx in x1[f1]:
+                for item in xx.keys():
+                    if f1 != item:
+                        x2 = {f2: []}
+                        x1[f1].append(x2)
+
+            if not x2[f2]:
+                x3 = {f3: []}
+                x2[f2].append(x3)
+
+                if '/' not in o3:
+                    x3[f3].append(o3)
+"""
+
+
+def foo(path_list):
+    for i in range(len(path_list)):
+        f1, o1 = path_list.pop().split('/', 1)
+
+        if not x:
+            x1 = {f1: []}
+            x1[f1].append(o1)
+            x.append(x1)
+        else:
+            for item in path_list:
+                if f1 not in item.keys():
+                    x1 = {f1: []}
+                    x1[f1].append(o1)
+                    path_list.append(x1)
+                else:
+                    path_list[f1].append(o1)
+
+
+foo(path_list)
+foo(x[0]['year_2018'])
 print(x)
